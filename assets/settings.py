66ea2471545732c12bb35baf121a41a38f743e71
xuyong <xuyong_bj@163.com>
2018-04-11 17:33:41 +0800

    修复日期显示异常

--- assets/settings.py
+++ assets/settings.py
@@ -74,13 +74,23 @@ WSGI_APPLICATION = 'assets.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fixed_assets',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': 'mysql.local.com',
+        'PORT': '3306',
     }
 }
-
 
 
 # Password validation
