f33f4eaa10b3431a6a25e4889b306c424138fa2c
xuyong <xuyong_bj@163.com>
2018-04-19 19:57:21 +0800

    个人资产查询功能新增序号

--- web/count.py
+++ web/count.py
@@ -34,13 +34,16 @@ def Getdata(request):
             else:
                 data_dic[res_data['assets_id']]['create_type'] = res_data['create_type']
                 data_dic[res_data['assets_id']]['create_date'] = res_data['create_date'].strftime('%Y-%m-%d')
+        num = 1
         for d in data_dic.keys():
             if data_dic[d]['create_type'] == "入库":
                 pass
             else:
                 if models.Asset.objects.values('asset_status').filter(assets_id=d)[0]['asset_status'] == 1:
                     data_d = {}
+                    data_d['id'] = num
                     data_d['user'], data_d['assets_id'], data_d['outdate'] = user,  '<a href="/assets/get_info?assets_id={}">{}</a>'.format(d, d), data_dic[d]['create_date']
                     data_d['assets_name'] = models.Asset.objects.values('assets_name').filter(assets_id=d)[0]['assets_name']
                     datalist.append(data_d)
+                    num += 1
         return HttpResponse(json.dumps(datalist))
--- web/static/assets/js/assets.js
+++ web/static/assets/js/assets.js
@@ -211,6 +211,7 @@ $(function(){
                     contentType : "application/x-www-form-urlencoded",
                     cache: false,
                     columns:[
+                        {field: 'id',title: '#序号'},
                         {field: 'assets_name',title: '资产名称'},
                         {field: 'assets_id',title: '资产编号'},
                         {field: 'outdate',title: '借用时间'}
