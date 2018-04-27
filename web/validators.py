ecee6c5d7a29757dd33bf9c084ba602da008bd09
xuyong <xuyong_bj@163.com>
2018-04-27 18:22:25 +0800

    简易密码

--- web/validators.py
+++ web/validators.py
@@ -7,5 +7,5 @@ from django.core.exceptions import ValidationError
 username_re = re.compile(r'^([a-zA-Z]{1}[\w]+?)$')
 username = RegexValidator(username_re, u'用户名需由字母数字或下划线组成,首字母为字母', 'invalid')
 
-password_re = re.compile(r'^[\w]+?$')
-password = RegexValidator(password_re,u'密码由字母数字下划线组成的字符串，最少为6位', 'invalid')
+password_re = re.compile(r'^.*$')
+password = RegexValidator(password_re, u'密码最少为6位', 'invalid')
