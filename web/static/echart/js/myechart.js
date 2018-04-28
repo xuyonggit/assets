16ba8fd93e8db8469c15038dc5c3fcdd3f80f4fe
xuyong <xuyong_bj@163.com>
2018-04-28 16:24:57 +0800

    首页图形优化：自适应大小

--- web/static/echart/js/myechart.js
+++ web/static/echart/js/myechart.js
@@ -9,43 +9,62 @@ $(function(){
             $("#echart").html("loadding");
         }
         }).done(function (result) {
+            var template = [];
             var datalist = [];
             for (i=0;i<result.length;i++){
                 datalist.push(result[i]);
+                template.push(result[i]['name'])
             }
-        // 基于准备好的dom，初始化echarts实例
         var myChart = echarts.init(document.getElementById('echart'));
 
         // 指定图表的配置项和数据
         var option = {
-            title: {
-                text: '库存状态一览',
-                x: 'center'
-            },
-            tooltip: {
-                trigger: 'item',
-                formatter: "{a} <br/>{b}: {c} ({d}%)"
-            },
-            legend: {
-                orient: 'vertical',
-                left: 'left',
-                data: ['库存中','使用中','故障','未知']
-            },
-            series: [{
-                name: '资产状态',
-                type: 'pie',
-                data: datalist,
-                itemStyle: {
-                    emphasis: {
-                        shadowBlur: 10,
-                        shadowOffsetX: 0,
-                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+            baseOption: {
+                title: {
+                    text: '库存状态一览',
+                    x: 'center'
+                },
+                tooltip: {
+                    trigger: 'item',
+                    formatter: "{a} <br/>{b}: {c} ({d}%)"
+                },
+                legend: {
+                    orient: 'vertical',
+                    left: 'left',
+                    data: template
+                },
+                toolbox: {
+                    show: true,
+                    feature : {
+                        mark : {show: true},
+                        dataView : {show: true, readOnly: false},
+                        magicType : {
+                            show: true,
+                            type: ['pie', 'funnel']
+                        },
+                    restore : {show: true},
+                    saveAsImage : {show: true}
                     }
-                }
-            }]
+                },
+                calculable : true,
+                series: [{
+                    name: '资产状态',
+                    type: 'pie',
+                    data: datalist,
+                    lableLine: {
+                        normal: {
+                            show: false
+                        },
+                        emphasis: {
+                            show: true
+                        }
+                    }
+                }]
+            }
         };
 
         // 使用刚指定的配置项和数据显示图表。
         myChart.setOption(option);
+        window.onresize = myChart.resize;
         })
-})
+});
