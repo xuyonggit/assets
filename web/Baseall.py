29e76f54a6f309e9b328eb306d29c75b35de8305
xuyong <xuyong_bj@163.com>
2018-07-26 18:09:26 +0800

    新增组织架构一览功能

--- web/Baseall.py
+++ web/Baseall.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
-from django.http.response import HttpResponse
 from django.contrib.auth.decorators import login_required
 from . import models
-from . import forms
-import json
 
 
 @login_required
